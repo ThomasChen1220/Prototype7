4d28b712a9802819e1ac922b8f016fab93a5a3bc
ThomasChen <canchen@usc.edu>
2020-10-14 13:48:43 +0800

    almost done

---
+++ Assets/Scripts/Crate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    public Sprite damaged;
+    public int life = 2;
+
+    public void Damage()
+    {
+        life--;
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (life < 2)
+        {
+            GetComponent<SpriteRenderer>().sprite = damaged;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
